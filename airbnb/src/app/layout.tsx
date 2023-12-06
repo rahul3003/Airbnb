e189cb5b5109f6a035afbaf4f5362581c83001bf
rahul@3003 <rahulkhandke71@gmail.com>
2023-12-06 20:34:05 +0530

    first commit

---
+++ airbnb/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/navbar/Navbar'
+
+const inter = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ airbnb/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="text-rose-500 text-2xl">
+    Hello,Airbnb
+    </main>
+  )
+}
